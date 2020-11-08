0103eb5ebe0ef7f835d0305cff2b86989b717d43
bozib <bboljat00@gmail.com>
2020-11-08 20:46:11 +0100

    proba

---
+++ build/precache-manifest.bf7a699bcc3c18356254d1fcd350192b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ff2dd64a5ad1600a914ba146681d9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67b8e2f22daba7c002b2",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5819f80f0fa8a21bfabc",
+    "url": "/static/css/main.59bbe6bf.chunk.css"
+  },
+  {
+    "revision": "67b8e2f22daba7c002b2",
+    "url": "/static/js/2.243e2c89.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.243e2c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5819f80f0fa8a21bfabc",
+    "url": "/static/js/main.a08a2d54.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  }
+]);
